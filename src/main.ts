9d88a32ee8046daf04d12c6ff08e147ccd18ec6b
AliAshraf222 <alialiali221019@gmail.com>
2025-06-28 21:22:46 +0300

    baisic auth

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  //set global prefix
+  const apiPrefix = 'api';
+  app.setGlobalPrefix(apiPrefix);
+
   await app.listen(process.env.PORT ?? 30000);
 }
 bootstrap()
